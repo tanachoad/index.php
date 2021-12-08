b8273e701d2ac4c8510c46d1e3b29870e381f53d
tanachoad <80100548+tanachoad@users.noreply.github.com>
2021-12-08 21:09:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,9 @@
     .athir{
         color: darkblue;
     }
+    td{
+        co;or: DodgerBlue;
+        }
     </style>
 </head>
 
@@ -64,7 +67,7 @@
                     console.log(item);
                     var line = "<tr>";
                     line += "<td>" + item.id + "</td>";
-                    line += "<td><b style="color:DodgerBlue;">" + item.title + "</b><br/>";
+                    line += "<td><b>" + item.title + "</b><br/>";
                     line += item.body + "</td>";
                     line += "<td> <button onClick='showDetails(" + item.id + ");' > link </button> </td>";
                     line += "</tr>";
